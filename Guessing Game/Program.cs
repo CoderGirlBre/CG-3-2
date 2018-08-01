82a5a45c8ee7a081782ba36787031948ab2ec9e3
CoderGirlBre <41300229+CoderGirlBre@users.noreply.github.com>
2018-07-31 21:56:48 -0500

    Add files via upload

--- Guessing Game/Program.cs
+++ Guessing Game/Program.cs
@@ -6,7 +6,24 @@ namespace Guessing_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //This Console.WriteLine is like the title.
+            Console.WriteLine("Guessing Game");
+
+            //This allows the user to input the number.
+            Console.Write("Pick a number 1-10: ");
+
+            //This reads the number the user entered and assigns it to a variable called userValue.
+            string userValue = Console.ReadLine();
+
+            //This is an Inline If statement asking "did the user enter 8?" It is also providing two options, "win" or "lose." If the user
+            //did enter 8, it will assign the first option (win) to the variable called message. If they did not enter 8, it would assign what's on the
+            // other side of the colon (lose).
+            string message = (userValue == "8") ? "win" : "lose";
+
+            //The {0} will be replaced with whatever option (win or lose) corresponds to what the user entered.
+            //What's in the quotes will be printed to the console and read.
+            Console.WriteLine("You {0}!", message);
+            Console.ReadLine();
         }
     }
 }
